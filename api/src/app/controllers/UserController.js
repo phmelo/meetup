449b2a81d54625d1ea4728b407925c5ca9b9fb3f
Paulo <phmelomorais@gmail.com>
2019-08-24 00:57:03 -0300

    Schema validation with yup

--- api/src/app/controllers/UserController.js
+++ api/src/app/controllers/UserController.js
@@ -1,7 +1,22 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const userExists = await User.findOne({
       where: {
         email: req.body.email,
@@ -22,6 +37,34 @@ class UserController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .min(6)
+        .when('password_new', (password_new, field) =>
+          password_new ? field.min(6).required() : field
+        ),
+
+      password_new: Yup.string().min(6),
+      password_confirm: Yup.string()
+        .min(6)
+        .when('password_new', (password_new, field) =>
+          password_new
+            ? field
+                .required()
+                .min(6)
+                .oneOf([Yup.ref('password_new')])
+            : field
+        ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const { name, email, password, password_new, password_confirm } = req.body;
 
     const user = await User.findByPk(req.userId);
