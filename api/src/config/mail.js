d482e4469d24027eb65d0ede9913306de27da943
Paulo <phmelomorais@gmail.com>
2019-09-04 23:13:29 -0300

    feature/Send-email

--- api/src/config/mail.js
+++ api/src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: ``,
-  port: ``,
+  host: 'smtp.mailtrap.io',
+  port: 2525,
   secure: false,
   auth: {
-    user: ``,
-    pass: ``,
+    user: 'e0a37847c4c4d6',
+    pass: 'c135f94e51803b',
   },
   default: {
     from: 'Meetup <noreply@meetapp.com>',
