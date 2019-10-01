2ec69e64920bc03c9f448ef2f3767bfa5f68d6d6
Paulo <phmelomorais@gmail.com>
2019-09-30 23:54:49 -0300

    feature: Adding Toasty Notifications

--- website/src/App.js
+++ website/src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 import './config/ReactotronConfig';
 import Routes from './routes';
 import history from './services/history';
 
 import store from './store';
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <Provider store={store}>
       <Router history={history}>
         <Routes />
+        <GlobalStyle />
+        <ToastContainer autoClose={3000} />
       </Router>
     </Provider>
   );
