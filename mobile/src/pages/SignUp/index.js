9c38ff32ab3af0e119d44f181f7ac924906214df
Paulo <phmelomorais@gmail.com>
2019-10-13 19:14:23 -0300

    feature: Adding signIn and signUp, and basic components

--- mobile/src/pages/SignUp/index.js
+++ mobile/src/pages/SignUp/index.js
@@ -1,12 +1,64 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useRef } from 'react';
+import { Image } from 'react-native';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.png';
 
-export default function SignUp() {
+import Background from '~/components/Background';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
+export default function SignUp({ navigation }) {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  function handleSubmit() {}
+
   return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
+    <Background>
+      <Container>
+        <Image source={logo} />
+        <Form>
+          <FormInput
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Nome completo"
+            returnKeyType="next"
+            onSubmitEditing={() => emailRef.current.focus()}
+          />
+          <FormInput
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+            ref={emailRef}
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
+          />
+          <FormInput
+            secureTextEntry
+            placeholder="Sua senha secreta"
+            ref={passwordRef}
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
+          />
+
+          <SubmitButton onPress={() => {}}>Criar conta</SubmitButton>
+        </Form>
+
+        <SignLink
+          onPress={() => {
+            navigation.navigate('SignIn');
+          }}
+        >
+          <SignLinkText>Já tenho login</SignLinkText>
+        </SignLink>
+      </Container>
+    </Background>
   );
 }
